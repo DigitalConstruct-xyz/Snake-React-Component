33ab2c6940a77e6ca3da16bf9a766302014c113f
monki1 <114178697+monki1@users.noreply.github.com>
2023-12-30 06:32:37 -0500

    refactor

--- index.js
+++ index.js
@@ -5,21 +5,17 @@ import useSnakeDirection from './Hooks/useSnakeDirection';
 import game from "./Game";
 import generateFood from "./Game/generateFood";
 
-const initialSnake = [[0, 0]];
-const initialDirection = 'RIGHT';
-
-
 
 const SnakeGame = ({
                        msPerFrame = 600,
                        gridSize = [70, 6],
-                       gridPixelSizeX = 8,
-                       gridPixelSizeY = 8,
-                       gridCellPixelSizeX = 6,
-                       gridCellPixelSizeY =6,
+                       gridPixelSize = [8, 8],
+                       gridCellPixelSize = [6,6],
                        backgroundColor = 'black',
                        snakeColor = 'yellow',
-                       foodColor = 'red'
+                       foodColor = 'red',
+                        initialDirection = 'RIGHT',
+                        initialSnake = [[0, 0]]
                    }) => {
     const [snake, setSnake] = useState(initialSnake);
     const initialFood = generateFood(initialSnake, gridSize)
@@ -35,9 +31,9 @@ const SnakeGame = ({
     }, msPerFrame);
 
     return <Grid gridSize={gridSize} snake={snake} food={food}
-        gridPixelSizeY={gridPixelSizeY}
-            gridCellPixelSizeX={gridCellPixelSizeX} gridCellPixelSizeY={gridCellPixelSizeY}
-            gridPixelSizeX={gridPixelSizeX}
+
+            gridCellPixelSizeX={gridCellPixelSize[0]} gridCellPixelSizeY={gridCellPixelSize[1]}
+            gridPixelSizeX={gridPixelSize[0]} gridPixelSizeY={gridPixelSize[1]}
             backgroundColor={backgroundColor}
             snakeColor={snakeColor}
             foodColor={foodColor}
