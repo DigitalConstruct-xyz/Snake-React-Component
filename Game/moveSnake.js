f67433d663bf60e2f7334003aace5809ca8aa8e7
monki1 <114178697+monki1@users.noreply.github.com>
2024-01-06 01:36:45 -0500

    clean up

--- Game/moveSnake.js
+++ Game/moveSnake.js
@@ -3,23 +3,6 @@ const moveSnake = (snake, direction) => {
     let newSnake = [...snake];
     let head = [...newSnake[newSnake.length - 1]];
 
-    // switch (direction) {
-    //     case 'RIGHT':
-    //         head = [head[0] + 1, head[1]];
-    //         break;
-    //     case 'LEFT':
-    //         head = [head[0] - 1, head[1]];
-    //         break;
-    //     case 'DOWN':
-    //         head = [head[0], head[1] + 1];
-    //         break;
-    //     case 'UP':
-    //         head = [head[0], head[1] - 1];
-    //         break;
-    //     default:
-    //         break;
-    // }
-
     head = [head[0]+direction[0], head[1]+direction[1]]
 
     newSnake.push(head);
